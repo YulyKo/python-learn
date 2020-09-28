beddb9c9e483cf3dfef5b59d806733f7212a7432
YulyKo <juliakotenko.forstudy@gmail.com>
2020-09-28 09:12:24 +0300

    contact book was done in c#

---
+++ Contact.py
@@ -0,0 +1,22 @@
+# class Contact():
+#     def _init_(self, contactName, phoneNumber):
+#         self.contactName = contactName
+#         self.phoneNumber = phoneNumber
+#         pass
+    
+#     def get_name(self):
+#         return self.contactName
+#         pass
+
+#     def get_number(self):
+#         return self.phoneNumber
+#         pass
+
+#     def set_name(self, name):
+#         self.contactName = name
+#         pass
+    
+#     def set_number(self, number):
+#         self.phoneNumber = number
+#         pass
+#     pass
